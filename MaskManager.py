0c5e81ac0bff9cd30fc61d438a244fb6d2b55e6c
layjain <layjain@mit.edu>
2021-03-21 04:32:21 -0400

    Quicker Camera

--- MaskManager.py
+++ MaskManager.py
@@ -2,6 +2,7 @@
 from picamera import PiCamera
 import main
 import time
+import os
 
 def identify_mask():
     #TODO: @Shinjini
@@ -11,16 +12,15 @@ def warn_user():
     #TODO: Pawan
     pass
 
+# camera = PiCamera()
+image_file = "capture.png"
+cmd = "raspistill --timeout 100 -o Desktop/capture.png"
 while True:
-    image_file = "capture.png"
-    camera = PiCamera()
-    camera.capture(image_file)
+    # camera.capture(image_file)
+    os.system(cmd)
     door_moving = main.get_movement()
     mask_on = identify_mask()
     if not mask_on and door_moving:
         warn_user()
     print(door_moving)
     time.sleep(0.3)
-
-
-
